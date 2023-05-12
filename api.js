b09ee943c986b2d9a9e4f0575a6fba10b2b22507
vinzeG2 <vinze.g2@gmail.com>
2023-05-11 20:42:22 -0400

    setup ready

---
+++ api.js
@@ -0,0 +1,17 @@
+import express from "express";
+import path from 'path';
+
+const app = express();
+
+app.use(express.json())
+
+app.use(express.static('app'))
+
+app.get('/', (req, res) => {
+    res.sendFile(`${path.resolve()}/index.html`)
+})
+
+app.listen('3000', () => {
+    console.log("The App is READY!");
+})
+
